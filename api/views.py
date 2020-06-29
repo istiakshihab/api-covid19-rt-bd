40b56a362d501a29e4998bba9b123ba835626684
istiakshihab <i.h.shihab@live.com>
2020-06-30 01:18:18 +0600

    before_rt edited

--- api/views.py
+++ api/views.py
@@ -38,11 +38,24 @@ def before_15_rt(request):
     json = finders.find('data/data.json')
     data = pd.read_json(json)
     data = data.sort_values(by=['Date'], ascending= False)
-    tempData = data.drop_duplicates('Date')
-    tempData = tempData[15:]
-    data = data.loc[data['Date'] == tempData['Date'].iloc[0]]
+    tempData15 = data.drop_duplicates('Date')
+    tempData15 = tempData15[15:]
+    tempData15 = data.loc[data['Date'] == tempData15['Date'].iloc[0]]
     data = data.drop_duplicates('district')
     data = data.sort_values(by=['ML'], ascending= False)
+    data = data[:15]
+    merged_df = data.merge(tempData15, how = 'outer', on = ['district'])
+    merged_df = merged_df[:15]
+    merged_df["ML_y"] = merged_df["ML_y"].fillna(0)
+    merged_df["Low_90_y"] = merged_df["Low_90_y"].fillna(0)
+    merged_df["High_90_y"] = merged_df["High_90_y"].fillna(0)
+    merged_df["Date_y"] = merged_df["Date_y"].fillna(0)
+    del merged_df["ML_x"]
+    del merged_df["Low_90_x"]
+    del merged_df["High_90_x"]
+    del merged_df["Date_x"]
+    data = merged_df
+    data.columns = ['district','Date','ML','Low','High']
     json = data.to_json(orient="records")
     return HttpResponse(json, content_type = 'application/json')
 
